b03e6af01234a96b4026250569416a0550dc1257
ChristianP5 <christiankps111@gmail.com>
2024-08-13 19:33:44 +0700

    rollback fix attempt #2

--- src/services/auth/saveRefreshToken.js
+++ src/services/auth/saveRefreshToken.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const RefreshTokens = require('../../models/RefreshTokens');
 
 const saveRefreshToken = async (refreshToken) => {
 
-    mongoose.connect(process.env.MONGODB_URL);
+    // mongoose.connect(process.env.MONGODB_URL);
 
     try{
         await RefreshTokens.create( {refreshToken: refreshToken} );
--- src/services/users/getUserByCredentials.js
+++ src/services/users/getUserByCredentials.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const Users = require('../../models/Users');
 
 const getUserByCredentials = async (username, password) => {
 
-    mongoose.connect(process.env.MONGODB_URL);
+    // mongoose.connect(process.env.MONGODB_URL);
 
     try{
         const result = await Users.findOne( {username: username, password: password} );
